6445953dd065ca86bd64f056d67891e2a57f6d07
stretch07 <78528552+stretch07@users.noreply.github.com>
2024-06-13 19:32:20 -0700

    fix: logging feature

--- src/core/request.ts
+++ src/core/request.ts
@@ -2,13 +2,13 @@ import { log } from "../util/logging.ts"
 
 export function sendRequest(data: any): Promise<any> {
     const callID = Math.random().toString().substring(2, 9)
-    console.log(`[41worker:work] (${callID}) Sent Request`, data);
+    log.debug(`[41worker:work] (${callID}) Sent Request`, data);
         return new Promise((resolve, reject) => {
             if (!data) reject("No data provided")
             self.onmessage = (event: MessageEvent) => {
                 // 0: data, 1: callID
                 if (event.data[1] === callID) {
-                    console.log(`[41worker:work] (${callID}) Received Response`, event.data[0]);
+                    log.debug(`[41worker:work] (${callID}) Received Response`, event.data[0]);
                     resolve(event.data[0])
                 } else {
                     self.postMessage(["Received " + event.data[0], event.data[1]]);
