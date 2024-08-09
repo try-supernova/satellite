ed5443d8408e4d96d1257fe7d1293297820fbda1
stretch07 <78528552+stretch07@users.noreply.github.com>
2024-08-08 20:51:17 -0700

    fix: logging feature

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {Process} from "./namespaces/Process/index.ts";
 import {UI} from "./namespaces/UI/index.ts";
+import {sendRequest} from "./core/request.js";
 
 export const Pluto = {
     Process,
     UI,
+    _core: {sendRequest},
 }
