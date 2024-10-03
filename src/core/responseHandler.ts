a9895a20d386019695a06b97c4f532e8a13f44f2
stretch07 <78528552+stretch07@users.noreply.github.com>
2024-10-02 23:16:19 -0700

    fix: stream handlers (new API)

--- src/core/responseHandler.ts
+++ src/core/responseHandler.ts
@@ -1,14 +1,16 @@
 import {EventEmitter} from "./eventEmitter.js"
+import {log} from "../util/logging.js";
 
 export class StreamHandler extends EventEmitter {
 	constructor() {
+        super()
 		self.onmessage = (event: MessageEvent) => {
             if (!event.data[1]) { //if the first index of the array exists, this originated from request-response based
             	//zeroth index: data
             	//first index: req-res callID (ignore)
 
                 log.debug(`[41worker:work] Received Streamed Event`, event.data[0]);
-                this.emit("stream", event.data[0])
+                super.emit("stream", event.data[0])
             }
         }
 	}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import {Process} from "./namespaces/Process/index.ts";
 import {UI} from "./namespaces/UI/index.ts";
 import {sendRequest} from "./core/request.js";
-import {streamHandler} from "./core/responseHandler.js";
+import {StreamHandler} from "./core/responseHandler.js";
 
 export const Pluto = {
     Process,
     UI,
-    _core: {sendRequest, streamHandler},
+    _core: {sendRequest, StreamHandler},
 }
