7e806eb0aa9da7c7d52bdf3a8055f9fc5bd7905c
443pablo <172942547+443pablo@users.noreply.github.com>
2024-10-09 17:41:12 -0700

    fix: no more declaration file, now we want a response handler apart from streams (requests originating from kernel)

--- src/core/responseHandler.ts
+++ src/core/responseHandler.ts
@@ -1,4 +1,4 @@
-import {EventEmitter} from "./eventEmitter.js"
+import {EventEmitter} from "../util/eventEmitter.js"
 import {log} from "../util/logging.js";
 
 export class StreamHandler extends EventEmitter {
@@ -11,6 +11,8 @@ export class StreamHandler extends EventEmitter {
 
                 log.debug(`[41worker:work] Received Streamed Event`, event.data[0]);
                 super.emit("stream", event.data[0])
+            } else { //idea: we need to have a global registry of call ids awaiting a response as to determine whether the message is a request or a response
+
             }
         }
 	}
